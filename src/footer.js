c6d136b5b2dac078ec3996b39916109307dca3f1
OlgaMilemis <89983348+OlgaMilemis@users.noreply.github.com>
2022-01-16 14:27:59 +0100

    added and erased files

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Weather.css";
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <a href="" rel="noreferrer" target="_blank">
+        Open source code
+      </a>{" "}
+      by Caro Gierich
+    </footer>
+  );
+}
